9b77384035d3ed0013bd61cb68ae30bc8cf53fa3
xia-66 <108531300+xia-66@users.noreply.github.com>
2024-12-31 16:55:15 +0800

    修改版权信息为自动更新及版本号更新

--- src/components/footer/Footer.vue
+++ src/components/footer/Footer.vue
@@ -1,7 +1,9 @@
 <template>
-  <div class=" footer px-5">
-    <div class="footer-copy"><span>&copy; 2024 All rights reserved. </span><a target="_blank"
-        href="https://beian.miit.gov.cn/">豫ICP备2023030596号</a> </div>
+  <div class="footer px-5">
+    <div class="footer-copy">
+      <span>&copy; {{ copyrightYears }} All rights reserved. </span>
+      <a target="_blank" href="https://beian.miit.gov.cn/">豫ICP备2023030596号</a>
+    </div>
     <div class="footer-powered">
       <span>Powered by &nbsp;</span>
       <a target="_blank" href="https://blog.heiyu.fun/">黑羽</a>
@@ -9,9 +11,19 @@
     </div>
   </div>
 </template>
+
 <script setup>
-import {RELEASE} from '@/config'
+import { RELEASE } from '@/config'
+import { computed } from 'vue'
+
+const START_YEAR = 2024
+const currentYear = new Date().getFullYear()
+
+const copyrightYears = computed(() => {
+  return currentYear > START_YEAR ? `${START_YEAR}-${currentYear}` : START_YEAR
+})
 </script>
+
 <style lang="scss" scoped>
 .footer {
   position: relative;
